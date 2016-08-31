a58377cdb273ce1971bff52807b6f13e7cb227d6
Jamie Bull <jamie.bull@oco-carbon.com>
2016-09-01 00:28:00 +0200

    Set up for Alpha release to PyPI

--- setup.py
+++ setup.py
@@ -1,67 +1,38 @@
-
-from setuptools import setup, find_packages
-from setuptools.command.test import test as TestCommand
-import io
-import codecs
-import os
-import sys
+from distutils.core import setup
 
 import geomeppy
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-def read(*filenames, **kwargs):
-    encoding = kwargs.get('encoding', 'utf-8')
-    sep = kwargs.get('sep', '\n')
-    buf = []
-    for filename in filenames:
-        with io.open(filename, encoding=encoding) as f:
-            buf.append(f.read())
-    return sep.join(buf)
-
-long_description = read('README.txt')
-
-class PyTest(TestCommand):
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        errcode = pytest.main(self.test_args)
-        sys.exit(errcode)
 
 setup(
     name='geomeppy',
-    version=geomeppy.__version__,
-    url='https://github.com/jamiebull1/geomeppy',
-    license='MIT License',
-    author='Jamie Bull',
-    tests_require=['pytest'],
-    cmdclass={'test': PyTest},
-    author_email='jamie.bull@oco-carbon.com',
-    description='Geometry editing for E+ idf files',
-    long_description=long_description,
     packages=['geomeppy',
               'devtools',
-              'tests'
+              'tests',
               ],
-    include_package_data=True,
+    version=geomeppy.__version__,
+    description='Geometry editing for E+ idf files',
+    author='Jamie Bull',
+    author_email='jamie.bull@oco-carbon.com',
+    url='https://github.com/jamiebull1/geomeppy',
+    download_url='https://github.com/jamiebull1/geomeppy/tarball/v0.0.1',
+    license='MIT License',
+    keywords=['EnergyPlus', 
+              'geometry',
+              'building performance simulation',
+              ],
     platforms='any',
-    test_suite='geomeppy.tests',
     install_requires = [
         "eppy>=0.5.2",
-        "numpy>=1.2.1",
+        "numpy>=1.11.1",
         "six>=1.10.0",  # python2/3 compatibility
-        "pyclipper>=1.0.2",  # used for geometry intersection
-        "transforms3d",  # used for geometry transformations
-        "matplotlib",  # for a simple geometry viewer
+        "pyclipper>=1.0.2",  # geometry intersection
+        "transforms3d>=0.3",  # geometry transformations
+        "matplotlib>=1.5.1",  # simple geometry viewer
         ],
     classifiers = [
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
-        'Development Status :: 4 - Beta',
+#        'Programming Language :: Python :: 3',  # on hold until Eppy updates
+        'Development Status :: 3 - Alpha',
         'Natural Language :: English',
         'Environment :: Console',
         'Intended Audience :: Science/Research',
@@ -69,7 +40,4 @@ setup(
         'Operating System :: OS Independent',
         'Topic :: Scientific/Engineering',
         ],
-    extras_require={
-        'testing': ['pytest'],
-    }
 )
