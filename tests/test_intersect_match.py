1a6d383d06418f9d846a3a09ed742819e277db7c
Jamie Bull <jamie.bull@oco-carbon.com>
2016-09-25 18:54:34 +0200

    Fix bugs in surface intersection and matching
    There are various places where it is advisable to round values or use
    almostequal to work around floating point inaccuracy.

--- tests/test_intersect_match.py
+++ tests/test_intersect_match.py
@@ -11,6 +11,11 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from geomeppy.recipes import translate
+from geomeppy.view_geometry import view_polygons
+
+from eppy.iddcurrent import iddcurrent
+from geomeppy.eppy_patches import IDF
 from geomeppy.intersect_match import get_adjacencies
 from geomeppy.intersect_match import getidfsurfaces
 from geomeppy.intersect_match import intersect
@@ -19,9 +24,6 @@ from geomeppy.intersect_match import is_hole
 from geomeppy.intersect_match import match_idf_surfaces
 from geomeppy.intersect_match import unique
 from geomeppy.polygons import Polygon3D
-
-from eppy.iddcurrent import iddcurrent
-from geomeppy.eppy_patches import IDF
 from six import StringIO
 
 
@@ -234,6 +236,34 @@ class TestSimpleTestPolygons():
         for poly in expected:
             assert poly in result
         
+    def test_vertically_offset(self):
+        """
+         ___
+        |_1_|
+        | 1 |
+        |_2_|
+        |_2_|
+         ___
+        |_1_|
+        | 2 |
+        |_3_|
+        |_4_|
+         
+        """
+        poly1 = Polygon3D([(0,0,1),(0,0,0),(2,0,0),(2,0,1)])
+        poly2 = Polygon3D([(3,0,1),(3,0,0),(1,0,0),(1,0,1)])
+        adjacencies = [(poly1, poly2)]
+        poly1 = Polygon3D([(0,0,1),(0,0,0),(1,0,0),(1,0,1)])
+        poly2 = Polygon3D([(3,0,1),(3,0,0),(2,0,0),(2,0,1)])
+        poly3 = Polygon3D([(1,0,1),(1,0,0),(2,0,0),(2,0,1)])
+        poly4 = Polygon3D([(2,0,1),(2,0,0),(1,0,0),(1,0,1)])
+        result = intersect(*adjacencies[0])
+        expected = [poly1, poly2, poly3, poly4]
+        assert len(result) == len(expected)
+        for poly in expected:
+            assert poly in result
+        
+        
 class TestMatchSurfaces():
 
     def setup(self):
@@ -292,6 +322,28 @@ def test_intersect():
     assert not is_hole(poly2, intersection)
 
 
+def test_real_intersect():
+    """
+    Test that we can make a previously failing test pass by moving to the
+    origin first.
+    
+    """
+    poly1 = Polygon3D(
+        [(526492.65, 185910.65, 6.0), (526492.65, 185910.65, 3.0),
+         (526489.05, 185916.45, 3.0), (526489.05, 185916.45, 6.0)])
+    poly2 = Polygon3D(
+        [(526489.05, 185916.45, 5.0), (526489.05, 185916.45, 2.5),
+        (526492.65, 185910.65, 2.5), (526492.65, 185910.65, 5.0)])
+    min_x = min(min(s.xs) for s in [poly1, poly2])
+    min_y = min(min(s.ys) for s in [poly1, poly2])
+    poly1, poly2 = translate([poly1, poly2], [-min_x, -min_y, 0])
+    intersection = translate(poly1.intersect(poly2), [min_x, min_y, 0])[0]
+    poly1, poly2 = translate([poly1, poly2], [min_x, min_y, 0])
+#    view_polygons({'blue': [poly1, poly2]})#, 'red': [intersection]})
+    assert not is_hole(poly1, intersection)
+    assert not is_hole(poly2, intersection)
+
+
 def test_is_hole():
     """Test if a surface represents a hole in one of the surfaces.
     """
@@ -339,6 +391,7 @@ class TestIntersectMatchRing():
         starting = len(idf.idfobjects['BUILDINGSURFACE:DETAILED'])
         intersect_idf_surfaces(idf)
         idf.set_default_constructions()
+        idf.view_model()
         print(idf.idfstr())
         ending = len(idf.idfobjects['BUILDINGSURFACE:DETAILED'])
         assert starting == 12
@@ -378,3 +431,46 @@ class TestIntersectMatch():
                      'z2_WALL_0004_2']:
             obj = idf.getobject('BUILDINGSURFACE:DETAILED', name)
             assert obj
+
+def test_real_scale():
+    """Test building, intersecting and matching from a real building footprint.
+    """
+    iddfhandle = StringIO(iddcurrent.iddtxt)
+    if IDF.getiddname() == None:
+        IDF.setiddname(iddfhandle)
+    
+    idf = IDF(StringIO('Version, 8.5;'))
+    poly1 = [(526492.65,185910.65),(526489.05,185916.45),
+             (526479.15,185910.3),(526482.65,185904.6),
+             (526492.65,185910.65)]
+    poly2 = [(526483.3,185903.15),(526483.5,185903.25),
+             (526482.65,185904.6),(526479.15,185910.3),
+             (526489.05,185916.45),(526492.65,185910.65),
+             (526493.4,185909.4),(526500,185913.95),
+             (526500.45,185914.3),(526500,185914.85),
+             (526497.4,185918.95),(526499.45,185920.2),
+             (526494.4,185928.35),(526466.05,185910.95),
+             (526471.1,185902.75),(526473.05,185903.9),
+             (526476.2,185898.8),(526479.95,185901.1),
+             (526483.3,185903.15)]
+    idf.add_block('small', poly1, 6.0, 2)
+    idf.add_block('large', poly2, 5.0, 2)
+    idf.translate_to_origin()
+    idf.intersect_match()
+    idf.set_wwr(0.25)
+    walls = idf.getsurfaces('wall')
+    # look for a wall which should have been split
+    assert 'Block large Storey 1 Wall 0003' not in [w.Name for w in walls]
+    # look for another wall which should have been split
+    assert 'Block large Storey 1 Wall 0005' not in [w.Name for w in walls]
+    # look for a wall which should be an internal wall
+    wall = idf.getobject(
+        'BUILDINGSURFACE:DETAILED', 'Block small Storey 1 Wall 0002_1')
+    assert wall.Outside_Boundary_Condition != 'outdoors'
+    # look for another wall which should be an internal wall
+    wall = idf.getobject(
+        'BUILDINGSURFACE:DETAILED', 'Block large Storey 1 Wall 0003_2')
+    assert wall.Outside_Boundary_Condition != 'outdoors'
+    idf.saveas('tmp.idf')
+
+    
