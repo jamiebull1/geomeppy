471b695c473d15f8f1b319fcf99a2b8d0a794cda
jamie.bull <jamie.bull@kiwi.com>
2018-01-11 23:06:14 +0100

    Change a release -> develop, then manually merge to master

--- release.py
+++ release.py
@@ -23,7 +23,7 @@ def replace(file_path, pattern, subst):
 
 def main():
     # check we're on master
-    assert b'* master' in subprocess.check_output(['git', 'branch']), 'Not on master branch'
+    assert b'* develop' in subprocess.check_output(['git', 'branch']), 'Not on develop branch'
     # check we're up-to-date
     status = subprocess.check_output(['git', 'status'])
     assert b'modified' not in status, 'Repository contains modified files'
@@ -50,10 +50,12 @@ def main():
         replace('setup.py', new_version, version)
         exit()
     try:
+        # push the changes
+        print(subprocess.check_output(['git', 'push', 'origin', 'develop']))
         # create a tagged release
         print(subprocess.check_output(['git', 'tag', '-m', 'release/%s' % new_version, 'v%s' % new_version]))
         # push to github
-        print(subprocess.check_output(['git', 'push', 'origin', 'master', '-f']))
+        print(subprocess.check_output(['git', 'push', 'origin', 'release/%s', '-f']))
     except Exception as e:
         # rollback
         print('rolling back tag')
