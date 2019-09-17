f5f4fe743c37b47322af38a1a5265036b95f9732
jamiebull1 <^tA2nq%K&F8TB&At>
2019-09-17 09:38:16 +0200

    Add docs/requirements.txt

--- tests/test_tutorial.py
+++ tests/test_tutorial.py
@@ -2,8 +2,8 @@ from geomeppy import IDF
 
 
 def test_tutorial():
-    IDF.setiddname("C:/EnergyPlusV8-8-0/Energy+.idd")
-    idf = IDF("C:/EnergyPlusV8-8-0/ExampleFiles/Minimal.idf")
+    IDF.setiddname("C:/EnergyPlusV9-1-0/Energy+.idd", testing=True)
+    idf = IDF("C:/EnergyPlusV9-1-0/ExampleFiles/Minimal.idf")
     idf.epw = "USA_CA_San.Francisco.Intl.AP.724940_TMY3.epw"
     idf.add_block(
         name="Boring hut", coordinates=[(10, 0), (10, 10), (0, 10), (0, 0)], height=3.5
