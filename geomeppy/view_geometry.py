db7189bdd33924be62f661dcdfb69f18c87cc47b
jamiebull1 <^tA2nq%K&F8TB&At>
2019-09-20 11:56:09 +0200

    fix: show windows in view_model

--- geomeppy/view_geometry.py
+++ geomeppy/view_geometry.py
@@ -84,7 +84,7 @@ def view_polygons(polygons):
 def _get_surfaces(idf):
     """Get the surfaces from the IDF.
     """
-    surfaces = idf.getsurfaces() + idf.getshadingsurfaces()
+    surfaces = idf.getsurfaces() + idf.getshadingsurfaces() + idf.getsubsurfaces()
     return surfaces
 
 
