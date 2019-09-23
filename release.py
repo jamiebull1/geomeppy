8a74391757272f3f9de58769aa89d26abc672db6
jamiebull1 <^tA2nq%K&F8TB&At>
2019-09-23 13:05:33 +0200

    Remove README.rst from release check

--- release.py
+++ release.py
@@ -59,7 +59,7 @@ def main(increment):
         print(subprocess.check_output(["git", "add", "geomeppy/__init__.py"]))
         print(subprocess.check_output(["git", "add", "setup.py"]))
         print(subprocess.check_output(["git", "add", "docs/source/conf.py"]))
-        print(subprocess.check_output(["git", "add", "README.rst"]))
+        print(subprocess.check_output(["git", "add", "README.md"]))
         print(
             subprocess.check_output(["git", "commit", "-m", "release/%s" % new_version])
         )
